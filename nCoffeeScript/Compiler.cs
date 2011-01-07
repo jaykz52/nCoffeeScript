2eece114af794509ce78fdef7cb1e5286989eb3f
Jason Kozemczak <jason.kozemczak@gmail.com>
2011-01-06 22:45:02 -0600

    Added src + VS solution/project

---
+++ nCoffeeScript/Compiler.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+namespace nCoffeeScript
+{
+    public static class Compiler
+    {
+        private const string scriptFileName = "coffee-script.js";
+        private static string coffeeScriptLibText;
+
+        public static void CompileFromFile(string filePath, bool noWrapOutput)
+        {
+            string textToCompile;
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                textToCompile = sr.ReadToEnd();
+            }
+            
+            // get the "name" of the coffeescript file and use that as the name for the compiled js
+            FileInfo fi = new FileInfo(filePath);
+            string compiledPath = fi.Directory + @"\" + fi.Name.Replace(".coffee", ".js");
+            
+            File.WriteAllText(compiledPath, Compile(textToCompile, noWrapOutput));
+        }
+
+        public static string Compile(string pCoffeeScriptInput, bool noWrapOutput)
+        {
+            // create a random file in the current dir so CScript can run it
+            string tempScriptFileName = CreateTempScriptFile(pCoffeeScriptInput, noWrapOutput);
+
+            // fire up CScript and redirect STDIN
+            ProcessStartInfo info = new ProcessStartInfo("Cscript", tempScriptFileName + " //Nologo");
+            info.UseShellExecute = false;
+            info.RedirectStandardOutput = true;
+            Process process = Process.Start(info);
+
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            // clean up
+            File.Delete(tempScriptFileName);
+
+            // give back the goods
+            return result;
+        }
+
+        private static string CreateTempScriptFile(string pInput, bool noWrapOutput)
+        {
+            // we need to get a "javascript-readable" bool string value for the wrapping option
+            string noWrapString = noWrapOutput.ToString().ToLower();
+
+            StringBuilder contentsStringBuilder = new StringBuilder();
+            contentsStringBuilder.Append(GetCoffeeScriptLib() + Environment.NewLine);
+            contentsStringBuilder.Append("var coffee_to_compile = \"\\" + Environment.NewLine);
+            contentsStringBuilder.Append(pInput.Replace("\r\n", "\\n\\" + Environment.NewLine));
+            contentsStringBuilder.Append("\";" + Environment.NewLine + Environment.NewLine);
+            contentsStringBuilder.Append("WScript.Echo(CoffeeScript.compile(coffee_to_compile, {no_wrap: " + noWrapString + "} ));");
+
+            string filePath = Guid.NewGuid() + ".js";   // this should be unique enough
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.Write(contentsStringBuilder.ToString());
+            }
+            return filePath;
+        }
+
+        private static string GetCoffeeScriptLib()
+        {
+            if (coffeeScriptLibText == null)
+            {
+                using (Stream s = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("nCoffeeScript." + scriptFileName))
+                {
+                    using (StreamReader sr = new StreamReader(s))
+                    {
+                        coffeeScriptLibText = sr.ReadToEnd();
+                    }
+                }
+            }
+            return coffeeScriptLibText;
+        }
+    }
+}
---
+++ nCoffeeScript/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace nCoffeeScript
+{
+    class Program
+    {
+        // TODO: I need to handle when the compiler can't compile!!!
+        // TODO: add code to handle a directory 
+        static void Main(string[] args)
+        {
+            bool noWrap = false;
+
+            // we need to loop through all of the args first to grab the "nowrap" arg
+            foreach (string arg in args)
+            {
+                if (arg == "/nowrap")
+                {
+                    noWrap = true;
+                    break;
+                }
+            }
+
+            // now that we have the important options, let's roll
+            foreach (string arg in args)
+            {
+                if (Directory.Exists(arg))
+                {
+                    // if we passed a directory, we'll compile each file and output it into a .js file in the directory
+                    DirectoryInfo di = new DirectoryInfo(arg);
+                    foreach (FileInfo fi in di.GetFiles("*.coffee"))
+                        Compiler.CompileFromFile(fi.FullName, noWrap);
+                }
+                else if (File.Exists(arg))
+                    Compiler.CompileFromFile(arg, noWrap);
+            }
+        }
+    }
+}
