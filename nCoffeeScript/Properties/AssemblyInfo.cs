8eef3639b6f72bea93a1d4b0c460aa6c0981de5e
Jason Kozemczak <jason.kozemczak@gmail.com>
2011-01-06 23:00:21 -0600

    Updated assembly details

--- nCoffeeScript/Properties/AssemblyInfo.cs
+++ nCoffeeScript/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("nCoffeeScript")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Coffeescript compiler for Windows.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jason Kozemczak")]
 [assembly: AssemblyProduct("nCoffeeScript")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright © 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
